4ca7f57816b8ccf70efa2a6f61efca16503d79f7
Sanath <77428228+sanathNU@users.noreply.github.com>
2021-12-09 17:58:15 +0530

    removed some redundant stuff

--- AAT assignments/6:1 Mux/Six_One_mux.v
+++ AAT assignments/6:1 Mux/Six_One_mux.v
@@ -3,25 +3,21 @@ input [5:0] In,
 input [2:0] Sel,
 output Out);
 
-//declaration of wires
 wire m1,m2,m3;
-wire n1,n2;
+wire n1;
 
 //the first layer of the multiplexer
 Mux21 a(In[0],In[1],Sel[0],m1);
 Mux21 b(In[2],In[3],Sel[0],m2);
-Mux21 c(In[4],In[5],Sel[0],m2);
+Mux21 c(In[4],In[5],Sel[0],m3);
 
 //the second layer of the multiplexer
-Mux21 a(m1,m2,Sel[1],n1);
-Mux21 a(m2,m3,Sel[1],n2);
-
+Mux21 d(m1,m2,Sel[1],n1);
 //the last and the final layer of the mux
-Mux21 a(n1,n2,Sel[2],Out);
+Mux21 f(n1,m3,Sel[2],Out);
 
 endmodule
 
-//defining Mux21 module seperately
 module Mux21(A,B,Sel,Out);
 input A,B,Sel;
 output Out;
